163c2e2ac362d75d3c1aafc243aebe85be8ecbc4
João Antônio Barbosa <joaobarbosadev@gmail.com>
2024-04-09 01:58:13 -0300

    add oil on nvim startup

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,11 +1,17 @@
 return {
   'stevearc/oil.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  opts = {},
+  opts = {
+    delete_to_trash = true,
+  },
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('oil').setup()
+    local oil = require 'oil'
+    oil.setup()
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+  end,
+  init = function()
+    require('oil').open()
   end,
 }
